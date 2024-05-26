88ae9529e3e18aa4107a63d35aabdd4dcc2ab83c
tangel90 <tangel90@protonmail.com>
2024-05-26 21:58:53 +0200

    configure nvchad terminals

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,18 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/nvchad/base46/"
 vim.g.mapleader = " "
 
+-- Check if PowerShell Core (pwsh) is available, otherwise fallback to Windows PowerShell
+if vim.fn.executable('pwsh') == 1 then
+  vim.opt.shell = 'pwsh'
+else
+  vim.opt.shell = 'powershell'
+end
+
+vim.opt.shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[\'Out-File:Encoding\']=\'utf8\';Remove-Alias -Force -ErrorAction SilentlyContinue tee;'
+vim.opt.shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+vim.opt.shellpipe = '2>&1 | %%{ "$_" } | tee %s; exit $LastExitCode'
+vim.opt.shellquote = ''
+vim.opt.shellxquote = ''
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
