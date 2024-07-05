1be94f0d0072964226d803476db00a8ede7c8db3
thomasergomed <thomas.schulz@ergomedplc.com>
2024-07-05 17:16:55 +0200

    recovery codes

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,10 +1,15 @@
 require "nvchad.mappings"
 
+
 -- add yours here
 
 local map = vim.keymap.set
 
 -- map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "kj", "<ESC>")
+map("i", "<C-d>", "<C-d>zz")
+map("i", "<C-u>", "<C-u>zz")
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
