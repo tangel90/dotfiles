829752f060ba5dd9e4a96626a534e79890355ff0
tangel <tangel90@protonmail.com>
2024-07-18 09:37:03 +0200

    add tmux navigator plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -130,7 +130,7 @@ require('lazy').setup({
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
-
+  { 'alexghergh/nvim-tmux-navigation' },
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
   --    require('gitsigns').setup({ ... })
