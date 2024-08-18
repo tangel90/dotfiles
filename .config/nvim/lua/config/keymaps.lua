e6ebb062cf0e9376a6555901a178ca92da92502c
= <thomas@pop-os.fritz.box>
2024-08-18 22:22:59 +0200

    add feline and <leader>d keymaps to nvim

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -8,7 +8,7 @@ map('n', 'n', 'nzzzv')
 map('n', 'N', 'Nzzzv')
 map('n', '<TAB>', ':bnext<CR>')
 map('n', '<S-TAB>', ':bprevious<CR>')
-map('n', '<leader>x', ':bdelete<CR>')
+map('n', '<leader>x', ':bdelete<CR>', { desc = 'Close current buffer' })
 map({ 'n', 'i', 'v' }, '<C-/>', '<Plug>(comment_toggle_linewise)')
 
 map({ 'n', 'i', 'v' }, '<C-s>', '<cmd> w <cr>')
