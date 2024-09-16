2b24bb813fd3fae5770604d11af3f56d3a137949
= <thomas@pop-os.fritz.box>
2024-09-16 23:52:54 +0200

    add kitty.conf

--- .config/nvim/lua/plugins/custom/molten-nvim.lua
+++ .config/nvim/lua/plugins/custom/molten-nvim.lua
@@ -1,10 +1,13 @@
 return {
   'benlubas/molten-nvim',
   config = function()
-    -- vim.keymap.set('n', '<localleader>mi', ':MoltenInit<CR>', { silent = true, desc = 'Initialize the plugin' })
-    -- vim.keymap.set('n', '<localleader>e', ':MoltenEvaluateOperator<CR>', { silent = true, desc = 'run operator selection' })
+    vim.keymap.set('n', '<localleader>mi', ':MoltenInit<CR>', { silent = true, desc = 'Initialize the plugin' })
+    vim.keymap.set('n', '<localleader>ro', ':MoltenEvaluateOperator<CR>', { silent = true, desc = 'run operator selection' })
     vim.keymap.set('n', '<localleader>rl', ':MoltenEvaluateLine<CR>', { silent = true, desc = 'evaluate line' })
     vim.keymap.set('n', '<localleader>rr', ':MoltenReevaluateCell<CR>', { silent = true, desc = 're-evaluate cell' })
     vim.keymap.set('v', '<localleader>r', ':<C-u>MoltenEvaluateVisual<CR>gv', { silent = true, desc = 'evaluate visual selection' })
+    vim.keymap.set('n', '<localleader>rd', ':MoltenDelete<CR>', { silent = true, desc = 'molten delete cell' })
+    vim.keymap.set('n', '<localleader>oh', ':MoltenHideOutput<CR>', { silent = true, desc = 'hide output' })
+    vim.keymap.set('n', '<localleader>os', ':noautocmd MoltenEnterOutput<CR>', { silent = true, desc = 'show/enter output' })
   end,
 }
