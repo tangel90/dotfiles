49a08a61cd059c82b56709aef6a933ce63c6f47b
tschulz <thomas.schulz@ergomedgroup.com>
2025-01-13 10:30:51 +0100

    change python to python3 in keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -9,20 +9,20 @@ map({ 'n', 'v' }, 'H', 'J')
 map({ 'n', 'v' }, '<C-e>', 'J')
 
 -- map({ 'n', 'i' }, '<C-x>', ':!dotnet run<CR>')
-map({ 'n' }, '<leader>xx', ':!python main.py<CR>', { desc = 'python main.py' })
-map({ 'n' }, '<leader>xm', ':!python main.py --settings_path=/mnt/d/projects/study_metrics/project_settings.json<CR>', { desc = 'Study [M]etrics Prod' })
+map({ 'n' }, '<leader>xx', ':!python3 main.py<CR>', { desc = 'python main.py' })
+map({ 'n' }, '<leader>xm', ':!python3 main.py --settings_path=/mnt/d/projects/study_metrics/project_settings.json<CR>', { desc = 'Study [M]etrics Prod' })
 map(
   { 'n' },
   '<leader>xd',
-  ':!python main.py --settings_path=/mnt/d/clinical_data_repository/modules/settings/etl_settings_dev.json<CR>',
+  ':!python3 main.py --settings_path=/mnt/d/clinical_data_repository/modules/settings/etl_settings_dev.json<CR>',
   { desc = 'EDC-Flex [D]ev' }
 )
-map({ 'n' }, '<leader>xpn', ':!python main.py --project NGGT002-P-2301<CR>', { desc = 'EDC-Flex [P]roject [N]ggt' })
-map({ 'n' }, '<leader>xpp', ':!python main.py --project PM-0059<CR>', { desc = 'EDC-Flex [P]roject [P]M-0059' })
+map({ 'n' }, '<leader>xpn', ':!python3 main.py --project NGGT002-P-2301<CR>', { desc = 'EDC-Flex [P]roject [N]ggt' })
+map({ 'n' }, '<leader>xpp', ':!python3 main.py --project PM-0059<CR>', { desc = 'EDC-Flex [P]roject [P]M-0059' })
 map(
   { 'n' },
   '<leader>xc',
-  ':!python main.py --settings_path=/mnt/d/clinical_data_repository/modules/settings/etl_settings_cyntegrity.json --project=PM-0059 --process=Cyntegrity<CR>',
+  ':!python3 main.py --settings_path=/mnt/d/clinical_data_repository/modules/settings/etl_settings_cyntegrity.json --project=PM-0059 --process=Cyntegrity<CR>',
   { desc = 'Etl [C]yntegrity' }
 )
 map('n', '<leader>y', 'yiw', { noremap = true, silent = true })
--- .zprofile
+++ .zprofile
@@ -45,7 +45,6 @@ alias vim='nvim'
 alias cat="bat"
 alias python="python3"
 alias lg="lazygit"
-alias act="conda activate"
 alias v="fd . --type f --hidden --exclude .git | fzf-tmux --border --preview='bat --style=numbers --color=always {}' -p 80%,80% | xargs nvim"
 alias chat="chatgpt"
 
