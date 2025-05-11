2f56abbe96970e1e2642cbc067c33ea7b4a0377a
tangel <tangel90@protonmail.com>
2025-05-11 18:54:54 +0200

    hyprland and waybar theme polish

--- .config/nvim/lua/plugins/rosepine.lua
+++ .config/nvim/lua/plugins/rosepine.lua
@@ -9,7 +9,7 @@ return { -- You can easily change to a different colorscheme.
     -- Load the colorscheme here.
     -- Like many other themes, this one has different styles, and you could load
     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-    vim.cmd.colorscheme 'rose-pine-main'
+    vim.cmd.colorscheme 'rose-pine-moon'
 
     -- You can configure highlights by doing something like:
     vim.cmd.hi 'Comment gui=none'
