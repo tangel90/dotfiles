06a8ccf92e844455f79067c0ed3d6136cda5ba3c
thomas schulz <thomas@TRI23504.trianel.de>
2026-01-13 16:53:07 +0100

    add python snippets

--- nvim/.config/nvim/lua/config/snippets.lua
+++ nvim/.config/nvim/lua/config/snippets.lua
@@ -3,15 +3,18 @@ local ls = require 'luasnip'
 --- python
 ls.add_snippets('python', {
   ls.parser.parse_snippet('new_pandas', 'import pandas as pd\n\ndef main():\n\t${1:}\n\treturn\n\nif __name__ == "__main__":\n\tmain()'),
+  ls.parser.parse_snippet('if_main', 'if __name__ == "__main__":\n\tmain()'),
 })
 
 ls.add_snippets('python', {
+  ls.parser.parse_snippet('lw', 'logger.warning("${1:}")'),
+  ls.parser.parse_snippet('lwf', 'logger.warning(f"${1:}")'),
+  ls.parser.parse_snippet('le', 'logger.error("${1:}")'),
+  ls.parser.parse_snippet('lef', 'logger.error(f"${1:}")'),
+  ls.parser.parse_snippet('ld', 'logger.debug("${1:}")'),
+  ls.parser.parse_snippet('ldf', 'logger.debug(f"${1:}")'),
   ls.parser.parse_snippet('li', 'logger.info("${1:}")'),
   ls.parser.parse_snippet('lif', 'logger.info(f"${1:}")'),
-})
-
-ls.add_snippets('python', {
-  ls.parser.parse_snippet('if_main', 'if __name__ == "__main__":\n\tmain()'),
 })
 
 --- go
