712b818bb5263940e4e99b6b97dc959a410b4404
thomas schulz <t.schulz@trianel.com>
2026-03-06 08:54:34 +0100

    update nvim obsidian version

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -12,7 +12,10 @@ return {
     --   "BufNewFile path/to/my-vault/*.md",
     -- },
     opts = {
+        ---@type obsidian.config.Internal
+        -- TODO: remove these in 4.0.0
         legacy_commands = false,
+
         workspaces = {
             {
                 name = 'personal',
@@ -36,155 +39,243 @@ return {
                 },
             },
         },
+        notes_subdir = nil,
+        new_notes_location = 'current_dir',
 
-        -- notes_subdir = 'notes',
-
-        -- Optional, set the log level for obsidian.nvim. This is an integer corresponding to one of the log
-        -- levels defined by "vim.log.levels.*".
         log_level = vim.log.levels.INFO,
+        -- Default random zettel IDs. To use readable UTF-8 slug IDs, set:
+        -- note_id_func = require("obsidian.builtin").title_id
+        --
+        ---@class obsidian.config.NoteOpts
+        ---
+        ---Default template to use, relative to template.folder or an absolute path.
+        ---The default looks like:
+        ---
+        ---```markdown
+        ------
+        ---id: {{id}}
+        ---aliases: []
+        ---tags: []
+        ------
+        ---```
+        ---
+        ---@field template string|?
+        note = {
+            template = (function()
+                local root = vim.iter(vim.api.nvim_list_runtime_paths()):find(function(path)
+                    return vim.endswith(path, 'obsidian.nvim')
+                end)
+                if not root then
+                    return nil
+                end
+                return vim.fs.joinpath(root, 'data/default_template.md')
+            end)(),
+        },
 
+        ---@class obsidian.config.FrontmatterOpts
+        ---
+        --- Whether to enable frontmatter, boolean for global on/off, or a function that takes filename and returns boolean.
+        ---@field enabled? (fun(fname: string?): boolean)|boolean
+        ---
+        --- Function to turn Note attributes into frontmatter.
+        ---@field func? fun(note: obsidian.Note): table<string, any>
+        --- Function that is passed to table.sort to sort the properties, or a fixed order of properties.
+        ---
+        ---@class obsidian.config.TemplateOpts
+        ---
+        ---@field enabled boolean|?
+        ---@field folder string|obsidian.Path|?
+        ---@field date_format string
+        ---@field time_format string
+        --- A map for custom variables, the key should be the variable and the value a function.
+        --- Functions are called with obsidian.TemplateContext objects and optional suffix strings.
+        --- See: https://github.com/obsidian-nvim/obsidian.nvim/wiki/Template#substitutions
+        ---@field substitutions table<string, string|fun(ctx: obsidian.TemplateContext, suffix: string|?):string|?>
+        ---@field customizations table<string, obsidian.config.CustomTemplateOpts>|?
+        templates = {
+            enabled = true,
+            folder = nil,
+            date_format = 'YYYY-MM-DD',
+            time_format = 'HH:mm',
+            substitutions = {
+                date = function(_, suffix)
+                    local format = suffix or Obsidian.opts.templates.date_format
+                    return require('obsidian.util').format_date(os.time(), format)
+                end,
+                time = function(_, suffix)
+                    local format = suffix or Obsidian.opts.templates.time_format
+                    return require('obsidian.util').format_date(os.time(), format)
+                end,
+                title = function(ctx)
+                    return ctx.partial_note and ctx.partial_note:display_name()
+                end,
+                id = function(ctx)
+                    return ctx.partial_note and ctx.partial_note.id
+                end,
+                path = function(ctx)
+                    return ctx.partial_note and tostring(ctx.partial_note.path)
+                end,
+            },
+
+            ---@class obsidian.config.CustomTemplateOpts
+            ---
+            ---@field notes_subdir? string
+            ---@field note_id_func? (fun(title: string|?, path: obsidian.Path|?): string)
+            customizations = {},
+        },
+
+        ---@class obsidian.config.BacklinkOpts
+        ---
+        ---@field parse_headers boolean
+        backlinks = {
+            parse_headers = true,
+        },
+
+        ---@class obsidian.config.CompletionOpts
+        ---
+        ---@field nvim_cmp? boolean
+        ---@field blink? boolean
+        ---@field min_chars? integer
+        ---@field match_case? boolean
+        ---@field create_new? boolean
+        completion = (function()
+            local has_nvim_cmp, _ = pcall(require, 'cmp')
+            local has_blink = pcall(require, 'blink.cmp')
+            return {
+                nvim_cmp = has_nvim_cmp and not has_blink,
+                blink = has_blink,
+                min_chars = 2,
+                match_case = true,
+                create_new = true,
+            }
+        end)(),
+
+        ---@class obsidian.config.PickerNoteMappingOpts
+        ---
+        ---@field new? string
+        ---@field insert_link? string
+
+        ---@class obsidian.config.PickerTagMappingOpts
+        ---
+        ---@field tag_note? string
+        ---@field insert_tag? string
+
+        ---@class obsidian.config.PickerOpts
+        ---
+        ---@field name obsidian.config.Picker|?
+        ---@field note_mappings? obsidian.config.PickerNoteMappingOpts
+        ---@field tag_mappings? obsidian.config.PickerTagMappingOpts
+        picker = {
+            name = nil,
+            note_mappings = {
+                new = '<C-x>',
+                insert_link = '<C-l>',
+            },
+            tag_mappings = {
+                tag_note = '<C-x>',
+                insert_tag = '<C-l>',
+            },
+        },
+
+        ---@class obsidian.config.SearchOpts
+        ---
+        ---@field sort_by string
+        ---@field sort_reversed boolean
+        ---@field max_lines integer
+        search = {
+            sort_by = 'modified',
+            sort_reversed = true,
+            max_lines = 1000,
+        },
+
+        ---@class obsidian.config.DailyNotesOpts
+        ---
+        ---@field enabled? boolean
+        ---@field folder? string
+        ---@field date_format? string
+        ---@field alias_format? string
+        ---@field template? string
+        ---@field default_tags? string[]
+        ---@field workdays_only? boolean
         daily_notes = {
-            -- Optional, if you keep daily notes in a separate directory.
-            -- folder = 'notes/dailies',
-            -- Optional, if you want to change the date format for the ID of daily notes.
-            date_format = '%Y-%m-%d',
-            -- Optional, if you want to change the date format of the default alias of daily notes.
-            alias_format = '%B %-d, %Y',
-            -- Optional, default tags to add to each new daily note created.
+            enabled = true,
+            folder = nil,
+            date_format = 'YYYY-MM-DD',
+            alias_format = nil,
             default_tags = { 'daily-notes' },
-            -- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
-            template = nil,
+            workdays_only = true,
         },
 
-        -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
-        completion = {
-            -- Set to false to disable completion.
-            nvim_cmp = true,
-            -- Trigger completion at 2 chars.
-            min_chars = 2,
+        ---@class obsidian.config.CallbackConfig
+        ---
+        ---Runs right after setup
+        ---@field post_setup? fun()
+        ---
+        ---Runs when entering a note buffer.
+        ---@field enter_note? fun(note: obsidian.Note)
+        ---
+        ---Runs when leaving a note buffer.
+        ---@field leave_note? fun(note: obsidian.Note)
+        ---
+        ---Runs right before writing a note buffer.
+        ---@field pre_write_note? fun(note: obsidian.Note)
+        ---
+        ---Runs anytime the workspace is set/changed.
+        ---@field post_set_workspace? fun(workspace: obsidian.Workspace)
+        callbacks = {},
+
+        ---@class obsidian.config.FooterOpts
+        ---
+        ---@field enabled? boolean
+        ---@field format? string
+        ---@field hl_group? string
+        ---@field separator? string|false Set false to disable separator; set an empty string to insert a blank line separator.
+        footer = {
+            enabled = true,
+            format = '{{backlinks}} backlinks  {{properties}} properties  {{words}} words  {{chars}} chars',
+            hl_group = 'Comment',
+            separator = string.rep('-', 80),
         },
 
-        -- Optional, configure key mappings. These are the defaults. If you don't want to set any keymappings this
-        -- way then set 'mappings = {}'.
-        -- mappings = {
-        --     -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-        --     ['gf'] = {
-        --         action = function()
-        --             return require('obsidian').util.gf_passthrough()
-        --         end,
-        --         opts = { noremap = false, expr = true, buffer = true, desc = 'Obsidian: gf_passthrough' },
-        --     },
-        --     ['<leader>og'] = {
-        --         action = '<cmd>Obsidian search<CR>',
-        --         opts = { buffer = true, desc = 'Obsidian: [G]rep in Notes' },
-        --     },
-        --     ['<leader>oo'] = {
-        --         action = '<cmd>Obsidian quick_switch<CR>',
-        --         opts = { buffer = true, desc = 'Obsidian: [O]pen QuickSwitch' },
-        --     },
-        --     ['<leader>ot'] = {
-        --         action = '<cmd>Obsidian toc<CR>',
-        --         opts = { buffer = true, desc = 'Obsidian: Show [T]oc' },
-        --     },
-        --     ['<leader>od'] = {
-        --         action = '<cmd>Obsidian dailies<CR>',
-        --         opts = { buffer = true, desc = 'Obsidian: Open [D]ailies' },
-        --     },
-        --     ['<leader>or'] = {
-        --         action = '<cmd>Obsidian rename<CR>',
-        --         opts = { buffer = true, desc = 'Obsidian: [R]ename Note' },
-        --     },
-        --     ['<leader>of'] = {
-        --         action = '<cmd>Obsidian tag<CR>',
-        --         opts = { buffer = true, desc = 'Obsidian: [F]ind by Tag' },
-        --     },
-        --     ['<leader>ow'] = {
-        --         action = '<cmd>Obsidian workspace<CR>',
-        --         opts = { buffer = true, desc = 'Obsidian: Select [W]orkspace' },
-        --     },
-        --     ['<leader>on'] = {
-        --         action = '<cmd>Obsidian new<CR>',
-        --         opts = { buffer = true, desc = 'Obsidian: [N]ew Note' },
-        --     },
-        --     ['<leader>oh'] = {
-        --         action = '<cmd>Obsidian help<CR>',
-        --         opts = { buffer = true, desc = 'Obsidian: [N]ew Note' },
-        --     },
-        --     -- Toggle check-boxes.
-        --     ['<leader><leader>'] = {
-        --         action = '<cmd>Obsidian toggle_checkbox<CR>',
-        --         opts = { buffer = true },
-        --     },
-        --     ['gl'] = {
-        --         action = '<cmd>Obsidian follow_link<CR>',
-        --         opts = { buffer = true, desc = 'Obsidian: Follow Link' },
-        --     },
-        --     -- Smart action depending on context, either follow link or toggle checkbox.
-        --     ['gd'] = {
-        --         action = function()
-        --             return require('obsidian').util.smart_action()
-        --         end,
-        --         opts = { buffer = true, expr = true, desc = 'Obsidian: Smart Action' },
-        --     },
-        --
-        --     ['<cr>'] = {
-        --         action = function()
-        --             return require('obsidian').util.smart_action()
-        --         end,
-        --         opts = { buffer = true, expr = true, desc = 'Obsidian: Smart Action' },
-        --     },
-        -- },
-        -- ui = {
-        --     enable = false, -- set to false to disable all additional syntax features
-        --     update_debounce = 200, -- update delay after a text change (in milliseconds)
-        --     max_file_length = 5000, -- disable UI features for files with more than this many lines
-        --     -- Define how various check-boxes are displayed
-        --     checkboxes = {
-        --         -- NOTE: the 'char' value has to be a single character, and the highlight groups are defined below.
-        --         [' '] = { char = '󰄱', hl_group = 'ObsidianTodo' },
-        --         ['x'] = { char = '', hl_group = 'ObsidianDone' },
-        --         ['>'] = { char = '', hl_group = 'ObsidianRightArrow' },
-        --         ['~'] = { char = '󰰱', hl_group = 'ObsidianTilde' },
-        --         ['!'] = { char = '', hl_group = 'ObsidianImportant' },
-        --         -- Replace the above with this if you don't have a patched font:
-        --         -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
-        --         -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
-        --
-        --         -- You can also add more custom ones...
-        --     },
-        --     -- Use bullet marks for non-checkbox lists.
-        --     bullets = { char = '•', hl_group = 'ObsidianBullet' },
-        --     external_link_icon = { char = '', hl_group = 'ObsidianExtLinkIcon' },
-        --     -- Replace the above with this if you don't have a patched font:
-        --     -- external_link_icon = { char = "", hl_group = "ObsidianExtLinkIcon" },
-        --     reference_text = { hl_group = 'ObsidianRefText' },
-        --     highlight_text = { hl_group = 'ObsidianHighlightText' },
-        --     tags = { hl_group = 'ObsidianTag' },
-        --     block_ids = { hl_group = 'ObsidianBlockID' },
-        --     hl_groups = {
-        --         -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
-        --         ObsidianTodo = { bold = true, fg = '#f78c6c' },
-        --         ObsidianDone = { bold = true, fg = '#89ddff' },
-        --         ObsidianRightArrow = { bold = true, fg = '#f78c6c' },
-        --         ObsidianTilde = { bold = true, fg = '#ff5370' },
-        --         ObsidianImportant = { bold = true, fg = '#d73128' },
-        --         ObsidianBullet = { bold = true, fg = '#89ddff' },
-        --         ObsidianRefText = { underline = true, fg = '#c792ea' },
-        --         ObsidianExtLinkIcon = { fg = '#c792ea' },
-        --         ObsidianTag = { italic = true, fg = '#89ddff' },
-        --         ObsidianBlockID = { italic = true, fg = '#89ddff' },
-        --         ObsidianHighlightText = { bg = '#75662e' },
-        --     },
-        -- },
-        -- Optional, by default when you use `:ObsidianFollowLink` on a link to an external
-        -- URL it will be ignored but you can customize this behavior here.
-        ---@param url string
-        -- follow_url_func = function(url)
-        --     vim.ui.open(url) -- need Neovim 0.10.0+
-        -- end,
+        ---@class obsidian.config.OpenOpts
+        ---
+        ---Opens the file with current line number
+        ---@field use_advanced_uri? boolean
+        ---
+        ---Function to do the opening, default to vim.ui.open
+        ---@field func? fun(uri: string)
+        ---
+        ---URI scheme whitelist, new values are appended to this list, and URIs with schemes in this list, will not be prompted to confirm opening
+        ---@field schemes? string[]
+        open = {
+            use_advanced_uri = false,
+            func = vim.ui.open,
+            schemes = { 'https', 'http', 'file', 'mailto' },
+        },
+
+        ---@class obsidian.config.CheckboxOpts
+        ---
+        ---@field enabled? boolean
+        ---
+        ---Order of checkbox state chars, e.g. { " ", "x" }
+        ---@field order? string[]
+        ---
+        ---Whether to create new checkbox on paragraphs
+        ---@field create_new? boolean
+        checkbox = {
+            enabled = true,
+            create_new = true,
+            order = { ' ', '~', '!', '>', 'x' },
+        },
+
+        ---@class obsidian.config.CommentOpts
+        ---@field enabled boolean
+        comment = {
+            enabled = false,
+        },
     },
-    config = function(_, opts)
-        local obsidian = require 'obsidian'
-        obsidian.setup(opts)
-    end,
+    -- config = function(_, opts)
+    --     local obsidian = require 'obsidian'
+    --     obsidian.setup(opts)
+    -- end,
 }
